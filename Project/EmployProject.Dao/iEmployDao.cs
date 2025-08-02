e66768afe8bd1b3b76d1c3d3d3e2e85679ea4b2e
pavan dharavath <97276341+thelooser15@users.noreply.github.com>
2025-08-02 15:16:07 +0530

    project

---
+++ Project/EmployProject.Dao/iEmployDao.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EmployProject.Models;
+
+namespace EmployProject.Dao
+{
+    internal interface iEmployDao
+    {
+        string AddEmployDao(Employ employ);
+        List<Employ> ShowEmployDao();
+        Employ SearchEmploy(int empno);
+    }
+}
---
+++ Project/EmplyProject.Bal/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("EmplyProject.Bal")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("EmplyProject.Bal")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("36c75f19-8cba-481b-8eea-c59c72e9ae1a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
